fd72965d431936ae7aafd4160fbd22c17c1304f2
Lisa Tiberio <ltiberio@gmail.com>
2017-11-17 10:28:11 -0500

    Created a small function to call readfcs_compensated_ERF and write the output data to the inputfilename with the extension changed to ".csv".

---
+++ code/@ColorModel/fcsToCsvFlowConverterFileWriter.m
@@ -0,0 +1,21 @@
+% Copyright (C) 2010-2017, Raytheon BBN Technologies and contributors listed 
+% in the AUTHORS file in TASBE analytics package distribution's top directory.
+%
+% This file is part of the TASBE analytics package, and is distributed
+% under the terms of the GNU General Public License, with a linking
+% exception, as described in the file LICENSE in the TASBE analytics
+% package distribution's top directory.
+
+% Call readfsc_compensated_MEFL to convert the data. Write the data to a
+% CSV file and return the data.  
+
+function data = fcsToCsvFlowConverterFileWriter(CM, filename, with_AF, floor)
+    if(CM.initialized<1), error('Cannot read MEFL: ColorModel not yet resolved'); end; % ensure initted
+    
+    data = readfcs_compensated_ERF(CM, filename, with_AF, floor);
+    [filepath,name,ext] = fileparts(filename);
+    csvName = [filepath name '.csv'];
+
+    csvwrite(csvName, data);
+end
+
